f65e108fc833e6c2fcf1f55bd61024ba180fdc68
chiochiwo <ccu12345@naver.com>
2021-02-05 15:55:06 +0900

    commit

--- mysite/pybo/urls.py
+++ mysite/pybo/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='index'),
 # ---------------------------------- [edit] ---------------------------------- #
-    path('<int:question_id>/', views.detail),
+    path('<int:question_id>/', views.detail,  name='detail'),
 # ---------------------------------------------------------------------------- #
 ]
