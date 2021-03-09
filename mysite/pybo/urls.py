4fe1bd4d118eef851997856e92f40279a8d3662b
chiochiwo <ccu12345@naver.com>
2021-03-09 19:44:02 +0900

    commit

--- mysite/pybo/urls.py
+++ mysite/pybo/urls.py
@@ -15,5 +15,6 @@ urlpatterns = [
     path('answer/create/<int:question_id>/', views.answer_create, name='answer_create'),
 # ---------------------------------------------------------------------------- #
 # ---------------------------------- [edit] ---------------------------------- #
+    path('question/create/', views.question_create, name='question_create'),
 
 ]
