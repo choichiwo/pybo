d7fd216c5a8f2dea21cf5dbd7c79638fd7446af9
chiochiwo <ccu12345@naver.com>
2021-03-24 15:38:11 +0900

    commit

--- mysite/pybo/views.py
+++ mysite/pybo/views.py
@@ -63,3 +63,4 @@ def question_create(request):
         form = QuestionForm()
         context = {'form': form}
         return render(request, 'pybo/question_form.html', context)
+# ---------------------------------------------------------------------------- #
