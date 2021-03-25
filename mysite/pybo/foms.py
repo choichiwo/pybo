e76f652a37a073950f587a8f3528190cd369970c
chiochiwo <ccu12345@naver.com>
2021-03-25 15:40:41 +0900

    commit

--- mysite/pybo/foms.py
+++ mysite/pybo/foms.py
@@ -1,6 +1,4 @@
-# ---------------------------------- [edit] ---------------------------------- #
 from django import forms
-from pybo.models import Question
 
 class QuestionForm(forms.ModelForm):
     class Meta:
